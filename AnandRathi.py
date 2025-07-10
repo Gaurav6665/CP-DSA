4505e879b02864c86c90c229426adb0d7b3833bb
Gaurav6665 <tiwari6665@gmail.com>
2025-07-10 18:48:57 +0530

    anand rathi interview questions

---
+++ AnandRathi.py
@@ -0,0 +1,62 @@
+# General Questions 
+
+# -What is REST Architecture.
+
+# -What are the components of API.
+
+# - What is caching. Name few caching servers.
+
+# -Difference between Synchronous and Asynchronous API call.
+
+# -What is a callback URL.
+
+# -What is difference between json loads and json dumps.
+
+# -What is benefit of using SQLAlchemy ORM vs raw sql queries.
+
+# -What do you mean by serverless architecture of AWS. What are its benefits.
+
+
+# Python Questions
+
+
+# - What is the difference between tuple and list.
+
+# - Write a script for factorial in most optimised way.
+
+
+# - What is list Comprehension and dictionary Comprehension.
+
+# - Write a squared list and dictionary as an output.
+
+
+# - Write a script in python which will print all the vowels from a given string.
+
+# vowels = "aeiou"
+
+# giving_string = "Hello!!"
+
+
+# - What is difference between .remove()  and .pop() functions of list.
+
+
+# - Write a script for bubble sort for the below array.
+# array = [44, 23, 56, 13, 29, 6, 68]
+
+
+# - Below is a list of dictionary. write a python script which will sort the list of dictionaries by the value of a specific key.
+
+# my_list = [{'name': 'Emp 1', 'age': 33}, {'Emp 2': 'Bob', 'age': 21}, {'name': 'Emp 3', 'age': 35}]
+
+
+# - Write a python script to split the array and add the first part to the end.
+
+# Input : arr[] = {12, 10, 5, 6, 52, 36}
+# k = 2
+# Output : arr[] = {5, 6, 52, 36, 12, 10}
+# Explanation : Split from index 2 and first part {12, 10} add to the end
+
+
+
+# - Write a script to find prime nos between a given range of integers, say between 10 and 60.
+
