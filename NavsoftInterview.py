7afd896da7e650627a0db0e64cb96e61a9b005d2
Gaurav Tiwari <tiwari6665@gmail.com>
2025-07-31 18:26:02 +0530

    Navsoft.in Interview Questions
    Interview by Vivek poddar and Alokbarna HR.
    Python dev Interview.

---
+++ NavsoftInterview.py
@@ -0,0 +1,86 @@
+#slice gaurav loop reversed
+
+name = "Gaurav"
+reversed_name = ""
+
+# for i in range(len(name)-1, -1,-1):
+#     reversed_name += name[i]
+
+# print("reversed_name=",reversed_name)
+
+# reversed_name = name[::-1]
+
+# for i in range(len(name)):
+#     if i % 2 ==0:
+#         print(name[i])
+
+# a= [1,2,3]
+# b =[1,2,3]
+
+
+# print(a==b)
+# print(a is b)
+
+# a = [1, 2, 3]
+#  b = a
+#  a.append(4)
+#  print(b)
+ 
+ 
+#  list para, 2nd highest num in list
+#  arr=[1,2,3,4] = 4,3,2,1
+
+# def Second_highest(arr):
+#     unique_arr = list   
+#     if len(arr)<2:
+#         return None
+    
+#     arr.sort(reverse=True)
+#     return arr[1]
+    
+# arr=[1,2,3,4,4]
+# print("second Highest = ",Second_highest(arr))
+
+# arr1=[1,2]
+# arr2=[3,4]
+
+# arr1.extend(arr2)
+
+# print("arr1=",arr1)
+
+
+# def decorator(func):
+#     def calling():
+#         print("before")
+#         func()
+#         print("after")
+#     return calling
+    
+# @decorator
+# def middle():
+#     print("middle")
+    
+# middle()
+
+# args, kward
+
+
+# og =[[1,2],[3,4]]
+
+# shallow = copy.copy(og)
+# deep = copy.deepcopy(og)
+
+# print("shallow=",shallow)
+# print("deep"=deep)
+
+# tbl 1, tbl 2, tbl3 has error
+
+
+
+
+    
+ 
+
+
+    
+    
